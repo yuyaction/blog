2ce2aa338a0fa2050fce6fd3a2516a93bcbddb33
yuya <yuyaction@gmail.com>
2020-04-05 22:52:07 +0900

    change admin site

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('blog', views.blog_list, name='blog_list'), 
     path('blog/tag', views.tag_list, name='tag_list'), 
     path('blog/<int:post_id>', views.post_content, name='post_content'),
-    path('blog/tag/<str:tag_name>', views.article_with_tag, name='awt')
+    path('blog/tag/<str:tag_name>', views.article_with_tag, name='awt'),
 ]
